fe2a33bc9e1606fc2cf6e423459bede33dedb10e
Kalen Hammann <kalen7@gmail.com>
2019-02-12 10:48:41 -0500

    first commit - set up Parse Server on AWS ec2

---
+++ Instagramclone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Instagramclone
+//
+//  Created by Kalen Hammann on 2/11/19.
+//  Copyright © 2019 Human Family. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
