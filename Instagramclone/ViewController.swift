24b9299f5440b78bf0105e0d3f7c14c9a8fe406b
Kalen Hammann <kalen7@gmail.com>
2019-02-12 12:07:39 -0500

    Save, retrieve, and update a comment

--- Instagramclone/ViewController.swift
+++ Instagramclone/ViewController.swift
@@ -14,6 +14,43 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /*
+        let comment = PFObject(className: "Comment")
+        
+        comment["text"] = "Nice shot!"
+        
+        comment.saveInBackground { (success, error) in
+            if (success) {
+                print("Save successful")
+            } else {
+                print("Save failed")
+            }
+        }
+        */
+        
+        /*
+        let query = PFQuery(className: "Comment")
+        query.getObjectInBackground(withId: "kwhTtBKEmb") { (object, eror) in
+            if let comment = object {
+                if let text = comment["text"] {
+                    print(text)
+                }
+            }
+        }
+        */
+        let query = PFQuery(className: "Comment")
+        query.getObjectInBackground(withId: "kwhTtBKEmb") { (object, eror) in
+            if let comment = object {
+                comment["text"] = "Yuck!"
+                comment.saveInBackground(block: { (success, error) in
+                    if (success) {
+                        print("Update succeeded")
+                    } else {
+                        print("Update failed")
+                    }
+                })
+            }
+        }
     }
 
 
