c811d0ff5a004346337534f61ee4421d9f766006
Kalen Hammann <kalen7@gmail.com>
2019-02-16 14:42:55 -0500

    Get posts for each user being followed

--- Instagramclone/FeedTableViewController.swift
+++ Instagramclone/FeedTableViewController.swift
@@ -7,17 +7,52 @@
 //
 
 import UIKit
+import Parse
 
 class FeedTableViewController: UITableViewController {
+    
+    var users = [String : String]()
+    var comments = [String]()
+    var usernames = [String]()
+    var imageFiles = [PFFileObject]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        let query = PFUser.query()
+        query?.whereKey("username", notEqualTo: PFUser.current()?.username)
+        query?.findObjectsInBackground(block: { (objects, error) in
+            if let users = objects {
+                for object in users {
+                    if let user = object as? PFUser {
+                        self.users[user.objectId!] = user.username!
+                    }
+                }
+            }
+            let getFollowedusersQuery = PFQuery(className: "Following")
+            getFollowedusersQuery.whereKey("follower", equalTo: PFUser.current()?.objectId)
+            getFollowedusersQuery.findObjectsInBackground(block: { (objects, error) in
+                if let followers = objects {
+                    for follower in followers {
+                        if let followedUser = follower["following"] {
+                            let query = PFQuery(className: "Post")
+                            query.whereKey("userId", equalTo: followedUser)
+                            query.findObjectsInBackground(block: { (objects, error) in
+                                if let posts = objects {
+                                    for post in posts {
+                                        self.comments.append(post["message"] as! String)
+                                        self.usernames.append(self.users[post["userId"] as! String]!)
+                                        self.imageFiles.append(post["imageFile"] as! PFFileObject)
+                                        self.tableView.reloadData()
+                                    }
+                                }
+                            })
+                        }
+                    }
+                }
+            })
+            
+        })
     }
 
     // MARK: - Table view data source
